647a12257da4567a4339745e36b11ff82f3d98a9
Dylan J <dylan@tritan.gg>
2022-06-14 22:00:21 -0400

    404 handling

---
+++ src/routes/handlers/404.ts
@@ -0,0 +1,31 @@
+import { Router, Request, Response } from "express";
+import { config } from "../../config";
+
+const route = Router();
+
+// 404 Handling
+route.use("*", (_req: Request, _res: Response) => {
+  let data = {
+    message: "The requested content could not be found on this server.",
+    error: true,
+    status: _res.status || 500,
+    version: config.version,
+    mode: config.production ? "production" : "development",
+    request: {
+      method: _req.method,
+      body: _req.body,
+      url: _req.originalUrl,
+      params: _req.params,
+      query: _req.query,
+      headers: _req.headers,
+      cookies: _req.cookies,
+      fresh: _req.fresh,
+      xhr: _req.xhr,
+      protocol: _req.protocol,
+    },
+  };
+
+  return _res.json(data);
+});
+
+export default route;
