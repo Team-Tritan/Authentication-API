e56964d560c8add8c7514de1c9cd5bc31804619e
Dylan J <dylan@tritan.gg>
2022-06-14 23:45:42 -0400

    login route

---
+++ src/routes/api/login.ts
@@ -0,0 +1,56 @@
+"use strict";
+
+import { Router, Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+import User from "../../libs/mongoose/User";
+
+let route = Router();
+
+route.post("/", async (_req: Request, _res: Response) => {
+  let { email, password } = _req.body;
+
+  if (!email || !password) {
+    return _res.status(400).json({
+      message: "BAD REQUEST: Login failed, missing required fields.",
+      error: true,
+      status: 400,
+    });
+  }
+
+  let _user = await User.findOne({ email: email });
+  let hash = _user.password;
+
+  await bcrypt.compare(password, hash, function (err, result) {
+    if (err) {
+      console.log("[AUTH SERVER] LOGIN FAILURE --> ", err);
+
+      return _res.status(500).json({
+        message: "INTERNAL SERVER ERROR: Password hash validation error.",
+        error: true,
+        status: 500,
+      });
+    }
+
+    if (result) {
+      console.log("[AUTH SERVER] LOGIN SUCCESS --> ", _user);
+      return _res.status(200).json({
+        message: "Authenticated Successfully",
+        code: 200,
+        error: false,
+        user: _user,
+      });
+    }
+
+    if (!result) {
+      console.log("[AUTH SERVER] LOGIN SUCCESS --> ", _user);
+      return _res.status(400).json({
+        message: "Incorrect password",
+        error: true,
+        status: 400,
+      });
+    }
+  });
+});
+
+export default route;
